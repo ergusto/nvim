b52aaa323a97c5b6e2ee0c5a8b181494b90e311c
Fergus Ruston <fergus.ruston@nearform.com>
2024-10-08 12:56:27 +0100

    organisation

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,52 +6,54 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
+local opt = vim.opt
+
 -- Make line numbers default
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
 -- Disable mouse
-vim.opt.mouse = ""
+opt.mouse = ""
 
 -- Hide the mode, since it's already in the status line
-vim.opt.showmode = false
+opt.showmode = false
 
 -- Enable break indent
-vim.opt.breakindent = true
+opt.breakindent = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
 
 -- Save undo history
-vim.opt.undofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
+opt.signcolumn = "yes"
 
 -- Decrease update time
-vim.opt.updatetime = 250
+opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
-vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live
-vim.opt.inccommand = "split"
+opt.inccommand = "split"
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Minimum number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+opt.scrolloff = 10
