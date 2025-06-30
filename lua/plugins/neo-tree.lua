dec9963cec0f7b7cf101875c6636c8f47ee66051
Fergus Ruston <fergus.ruston@virginmediao2.co.uk>
2025-06-30 22:15:05 +0100

    everforest

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  cmd = "Neotree",
+  opts = {
+    fielsystem = {
+      filtered_items = {
+        hide_gitignored = false,
+      },
+    },
+  },
+}
