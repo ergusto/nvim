77a5657c249f9b256b853f11a276dbfa1356e20d
Fergus Ruston <fergus.ruston@gmail.com>
2025-07-21 14:32:11 +0100

    remove old colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,5 @@
 return {
   -- Colorscheme
-  -- {
-  --   "sainnhe/everforest",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     -- Optionally configure and load the colorscheme
-  --     -- directly inside the plugin declaration.
-  --     vim.g.everforest_enable_italic = true
-  --     vim.g.everforest_background = "hard"
-  --     vim.cmd.colorscheme("everforest")
-  --   end,
-  -- },
   {
     "ergusto/pizazz.nvim",
     lazy = false,
