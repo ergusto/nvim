89d9bc6e2fde54ee390c61e2683fe505ae7c57be
Fergus Ruston <fergus.ruston@virginmediao2.co.uk>
2025-07-21 09:12:35 +0100

    feat: pizazz.nvim theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,26 @@
 return {
   -- Colorscheme
+  -- {
+  --   "sainnhe/everforest",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.everforest_enable_italic = true
+  --     vim.g.everforest_background = "hard"
+  --     vim.cmd.colorscheme("everforest")
+  --   end,
+  -- },
   {
-    "sainnhe/everforest",
+    "ergusto/pizazz.nvim",
     lazy = false,
     priority = 1000,
+    dependencies = {
+      { "rktjmp/lush.nvim", lazy = true },
+    },
     config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.everforest_enable_italic = true
-      vim.g.everforest_background = "hard"
-      vim.cmd.colorscheme("everforest")
+      vim.cmd.colorscheme("pizazz_dark")
     end,
   },
 }
